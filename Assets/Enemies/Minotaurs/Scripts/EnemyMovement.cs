93d6d414e5b6c9d07845a2c339435eda0aaaf71b
Barbito Sebastian <sebastianbarbito0,qgmail.com>
2025-11-01 21:25:02 -0300

    Feat: Implementacion del movimiento del Player mediante las teclas y se creo un area del enemigo que le permite moverse si el player se acerca

---
+++ Assets/Enemies/Minotaurs/Scripts/EnemyMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Transform player;
+    public float detectionRadious = 0.5f;
+    public float speed = 2;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private bool enMovimiento;
+    private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position); 
+
+        if(distanceToPlayer < detectionRadious)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            if (direction.x < 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            if (direction.x < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+
+            movement = new Vector2(direction.x, 0);
+            enMovimiento = true;
+        }
+        else
+        {
+            movement = Vector2.zero;
+            enMovimiento = false;
+        }
+
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+       
+        animator.SetBool("enMovimiento", enMovimiento);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector2 direccionDanio = new Vector2(transform.position.x, 0);
+
+            collision.gameObject.GetComponent<Player_Movement>().RecibeDanio(direccionDanio, 1);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadious);
+    }
+}
--- Assets/Player/Scrips/Player_Movement.cs
+++ Assets/Player/Scrips/Player_Movement.cs
@@ -4,11 +4,19 @@ public class Player_Movement : MonoBehaviour
 {
     public float velocidad = 5f;
 
+    public float fuerzaSalto = 10f;
+    public float fuerzaRebote = 10f;
+    public float longitudRaycast = 1.0f;
+    public LayerMask capSuelo;
+
+    private bool enSuelo;
+    private bool recibiendoDanio;
+    private Rigidbody2D rb;
     public Animator animator;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -17,17 +25,52 @@ public class Player_Movement : MonoBehaviour
 
         animator.SetFloat("Movement", velocidadX * velocidad);
 
-        if(velocidadX < 0)
+        if (velocidadX < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        if(velocidadX > 0)
+        if (velocidadX > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
 
         Vector3 posicion = transform.position;
 
+        if(!recibiendoDanio)
+
         transform.position = new Vector3(posicion.x + velocidadX, posicion.y, posicion.z);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capSuelo);
+        enSuelo = hit.collider != null;
+
+        if (enSuelo && Input.GetKeyDown(KeyCode.Space) && !recibiendoDanio)
+        {
+            rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+        }
+        animator.SetBool("ensuelo", enSuelo);
+        animator.SetBool("recibeDanio", recibiendoDanio);
     }
-}
+
+    public void RecibeDanio(Vector2 direccion, int cantDanio)
+    {
+        if(!recibiendoDanio)
+        {
+            recibiendoDanio = true;
+            Vector2 rebote = new Vector2(transform.position.x - direccion.x, 1).normalized;
+            rb.AddForce(rebote * fuerzaRebote, ForceMode2D.Impulse);
+        }
+        
+    }
+
+    public void DesactivaDanio()
+    {
+        recibiendoDanio = false;
+        rb.linearVelocity = Vector2.zero;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * longitudRaycast);
+    }
+
+    }
