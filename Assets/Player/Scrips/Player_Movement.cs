427dbafbabf0b3f83446d98299093ef72af0bf0e
Barbito Sebastian <sebastianbarbito0,qgmail.com>
2025-11-02 20:03:19 -0300

    Implentacion del ataque del Player y el enemigo/ implementacion de muerte de ambos y destruccion del enemigo cuando pasa 4 segundos despues de morrir

--- Assets/Player/Scrips/Player_Movement.cs
+++ Assets/Player/Scrips/Player_Movement.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     public float velocidad = 5f;
+    public float vida = 3;
 
     public float fuerzaSalto = 10f;
-    public float fuerzaRebote = 10f;
+    public float fuerzaRebote = 5f;
     public float longitudRaycast = 1.0f;
     public LayerMask capSuelo;
 
     private bool enSuelo;
     private bool recibiendoDanio;
+    private bool atacando;
+    private bool atacando_02;
+    public bool muerto;
     private Rigidbody2D rb;
     public Animator animator;
 
@@ -20,6 +24,51 @@ public class Player_Movement : MonoBehaviour
     }
 
     void Update()
+    {
+
+        if(! muerto)
+        {
+
+            if (!atacando || atacando_02)
+            {
+                Movimiento();
+
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capSuelo);
+                enSuelo = hit.collider != null;
+
+                if (enSuelo && Input.GetKeyDown(KeyCode.Space) && !recibiendoDanio)
+                {
+                    rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+                }
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                Atacando();
+            }
+
+            if (Input.GetKeyDown(KeyCode.K) && !atacando_02 && enSuelo)
+            {
+                Atacando();
+            }
+
+            if(Input.GetKeyDown(KeyCode.L))
+            {
+                Atacando_02();
+            }
+            if (Input.GetKeyDown(KeyCode.L) && !atacando && enSuelo)
+            {
+                Atacando_02();
+            }
+        }
+        
+
+        Animaciones();
+
+    }
+
+    public void Movimiento()
     {
         float velocidadX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidad;
 
@@ -36,19 +85,30 @@ public class Player_Movement : MonoBehaviour
 
         Vector3 posicion = transform.position;
 
-        if(!recibiendoDanio)
+        if (!recibiendoDanio)
 
-        transform.position = new Vector3(posicion.x + velocidadX, posicion.y, posicion.z);
+            transform.position = new Vector3(posicion.x + velocidadX, posicion.y, posicion.z);
+    }
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capSuelo);
-        enSuelo = hit.collider != null;
-
-        if (enSuelo && Input.GetKeyDown(KeyCode.Space) && !recibiendoDanio)
-        {
-            rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
-        }
+    public void Animaciones()
+    {
         animator.SetBool("ensuelo", enSuelo);
         animator.SetBool("recibeDanio", recibiendoDanio);
+        animator.SetBool("Atacando", atacando);
+        animator.SetBool("muerto", muerto);
+        animator.SetBool("Atacando_02", atacando_02);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hacha"))
+        {
+            float fuerzaExtra = 2f; //  más alto = más empuje
+            Vector2 direccion = new Vector2(collision.gameObject.transform.position.x, 0);
+            fuerzaRebote *= fuerzaExtra;
+            RecibeDanio(direccion, 2);
+            fuerzaRebote /= fuerzaExtra; //  vuelve al valor normal
+        }
+
     }
 
     public void RecibeDanio(Vector2 direccion, int cantDanio)
@@ -56,8 +116,17 @@ public class Player_Movement : MonoBehaviour
         if(!recibiendoDanio)
         {
             recibiendoDanio = true;
-            Vector2 rebote = new Vector2(transform.position.x - direccion.x, 1).normalized;
-            rb.AddForce(rebote * fuerzaRebote, ForceMode2D.Impulse);
+            vida -= cantDanio;
+            if (vida <=0)
+            {
+                muerto = true;
+            }
+            if (!muerto)
+            {
+                Vector2 rebote = new Vector2(transform.position.x - direccion.x, 1).normalized;
+                rb.AddForce(rebote * fuerzaRebote, ForceMode2D.Impulse);
+            }
+            
         }
         
     }
@@ -66,6 +135,26 @@ public class Player_Movement : MonoBehaviour
     {
         recibiendoDanio = false;
         rb.linearVelocity = Vector2.zero;
+    }
+
+    public void Atacando()
+    {
+        atacando = true;
+    }
+
+    public void DesactivaAtaque()
+    {
+        atacando = false;
+    }
+
+    public void Atacando_02()
+    {
+        atacando_02 = true;
+    }
+
+    public void DesactivaAtaque_02()
+    {
+        atacando_02 = false;
     }
     void OnDrawGizmos()
     {
